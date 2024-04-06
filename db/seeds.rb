fbdd78b7ec177ce1a072d50820a6abc300d6646b
Florent Vandroy <florentvandroy@gmail.com>
2024-04-06 14:58:13 +0200

    Add first_name and last_name to the user, with users seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,7 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+User.destroy_all
+
+User.create(first_name: 'Florent', last_name: 'Vandroy', email: 'florentvandroy@gmail.com', password: 'password')
